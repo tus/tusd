186e192cb0f0738fafc7bdba6f5011177a979cf9
Kevin van Zonneveld <kevin@vanzonneveld.net>
2015-08-19 16:34:49 +0200

    Move infra builder to own repo

--- envs/development/config.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-__envdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source ${__envdir}/../_default.sh 2>/dev/null || source ${__envdir}/_default.sh
-
-export DEPLOY_ENV="development"
-export NODE_ENV="development"
-export DEBUG=tsd:*,express:application
--- envs/test/config.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-__envdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source ${__envdir}/../_default.sh 2>/dev/null || source ${__envdir}/_default.sh
-
-export DEPLOY_ENV="test"
-export NODE_ENV="test"
-export DEBUG="tsd:*"
