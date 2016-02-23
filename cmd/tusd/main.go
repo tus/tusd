0a43ce5663215d2bc0f6fd8474d1337634c34959
Marius <Acconut@users.noreply.github.com>
2016-02-23 14:36:36 +0100

    Update greeting

--- cmd/tusd/main.go
+++ cmd/tusd/main.go
@@ -76,7 +76,7 @@ Congratulations for setting up tusd! You are now part of the chosen elite and
 able to experience the feeling of resumable uploads! We hope you are as excited
 as we are (a lot)!
 
-However, there is something I want to tell you personally: While you got tusd
+However, there is something you should be aware of: While you got tusd
 running (you did an awesome job!), this is the root directory of the server
 and tus requests are only accepted at the %s route.
 
