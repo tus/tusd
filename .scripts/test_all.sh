8f5782b73a239b8b746f8c4ef8dbfb10f9c0a307
Marius <maerious@gmail.com>
2016-11-11 21:42:35 +0100

    Run consul tests on only Go1.7+

--- .scripts/test_all.sh
+++ .scripts/test_all.sh
@@ -5,11 +5,12 @@ set -e
 # Find all packages containing Go source code inside the current directory
 packages=$(find ./ -maxdepth 2 -name '*.go' -printf '%h\n' | sort | uniq)
 
-# The consul package only supports Go1.6+ and therefore we will only run the
+# The consul package only supports Go1.7+ and therefore we will only run the
 # corresponding tests on these versions.
 goversion=$(go version)
 if [[ "$goversion" == *"go1.4"* ]] ||
-   [[ "$goversion" == *"go1.5"* ]]; then
+   [[ "$goversion" == *"go1.5"* ]] ||
+   [[ "$goversion" == *"go1.6"* ]]; then
 
   echo "Skipping tests requiring Consul which is not supported on $goversion"
 
