652a003f8f0e6ff3d507b0d1d306bcb41198c1f3
kiloreux <kiloreux@gmail.com>
2017-11-28 18:45:43 +0000

    Deploy to production

--- .scripts/deploy_gcloud.sh
+++ .scripts/deploy_gcloud.sh
@@ -14,3 +14,34 @@ docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
 docker push kiloreux/tusd:$TRAVIS_COMMIT;
 docker push kiloreux/tusd:latest;
 
+echo $GCLOUD_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
+gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+
+gcloud --quiet config set project $PROJECT_NAME
+gcloud --quiet config set container/cluster $CLUSTER_NAME
+gcloud --quiet config set compute/zone ${COMPUTE_ZONE}
+gcloud --quiet container clusters get-credentials $CLUSTER_NAME
+
+kubectl config current-context
+
+helm init --service-account tiller --upgrade
+
+kubectl apply -f "${__root}/.infra/kube/00-namespace.yaml"
+kubectl apply -f "${__root}/.infra/kube/deployment.yaml"
+kubectl apply -f "${__root}/.infra/kube/service.yaml"
+kubectl apply -f "${__root}/.infra/kube/ingress-tls.yaml"
+
+kubectl set image deployment/tusd --namespace=tus tusd=docker.io/kiloreux/tusd:$TRAVIS_COMMIT
+
+kubectl get pods --namespace=tus
+kubectl get service --namespace=tus
+kubectl get deployment --namespace=tus
+
+
+function cleanup {
+    printf "Cleaning up...\n"
+    rm -vf "${HOME}/gcloud-service-key.json"
+    printf "Cleaning done."
+}
+
+trap cleanup EXIT
