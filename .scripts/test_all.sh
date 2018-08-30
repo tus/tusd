3e41283188da2e921ac165815df3f475b6c2fea6
Anders Chen <anders@wistia.com>
2018-08-29 17:10:12 -0700

    Etcd test to only run on go1.9 and go1.10

--- .scripts/test_all.sh
+++ .scripts/test_all.sh
@@ -21,22 +21,32 @@ if [[ "$goversion" == *"go1.5"* ]] ||
 
   echo "Skipping tests requiring GCSStore, which is not supported on $goversion"
   packages=$(echo "$packages" | sed '/gcsstore/d')
-
-  echo "Skipping tests requiring etcd3locker, which is not supported on $goversion"
-  packages=$(echo "$packages" | sed '/etcd3locker/d')
 else
   # Install the Consul packages which are not vendored.
   go get -u github.com/hashicorp/consul/...
+fi
 
-  # Install the etcd packages which are not vendored.
+install_etcd_pkgs() {
   go get -u github.com/coreos/etcd
-  # use release 3.3 as master branches are not stable for etcd
-  (cd ../../coreos/etcd && git fetch origin && git checkout release-3.3)
   go get -u google.golang.org/grpc
   go get -u github.com/coreos/go-semver
   go get -u github.com/ugorji/go/codec
   go get -u github.com/mwitkow/go-etcd-harness
+}
 
+# The etcd 3.3.x package only supports Go1.9+ and therefore
+# we will only run the corresponding tests on these versions.
+if [[ "$goversion" == *"go1.5"* ]] ||
+   [[ "$goversion" == *"go1.6"* ]] ||
+   [[ "$goversion" == *"go1.7"* ]] ||
+   [[ "$goversion" == *"go1.8"* ]]; then
+  echo "Skipping tests requiring etcd3locker, which is not supported on $goversion"
+  packages=$(echo "$packages" | sed '/etcd3locker/d')
+else
+  # Install the etcd packages which are not vendored.
+  install_etcd_pkgs
+  # use release 3.3 as master branches are not stable for etcd
+  (cd ../../coreos/etcd && git fetch origin && git checkout release-3.3)
 fi
 
 # Install the AWS SDK and Prometheus client which is explicitly not vendored
