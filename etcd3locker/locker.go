afe6887aed00c0d19e1720011626fe31f728cbad
Anders Chen <anders@wistia.com>
2018-11-07 22:33:23 +0100

    Add more extensive package overview / docs

--- etcd3locker/locker.go
+++ etcd3locker/locker.go
@@ -1,4 +1,45 @@
+// Package etcd3locker provides a locking mechanism using an etcd3 cluster
+//
+// To initialize a locker, a pre-existing connected etcd3 client must be present
+//
+//		client, err := clientv3.New(clientv3.Config{
+//				Endpoints:   []string{harness.Endpoint},
+//				DialTimeout: 5 * time.Second,
+//		})
+//
+//	For the most basic locker (e.g. non-shared etcd3 cluster / use default TTLs),
+//	a locker can be instantiated like the following:
+//
+//		locker, err := etcd3locker.New(client)
+//		if err != nil {
+//				return nil, fmt.Errorf("Failed to create etcd locker: %v", err.Error())
+//		}
+//
+//	The locker will need to be included in composer that is used by tusd:
+//
+//		composer := tusd.NewStoreComposer()
+//		locker.UseIn(composer)
+//
+//	For a shared etcd3 cluster, you may want to modify the prefix that etcd3locker uses:
+//
+//		locker, err := etcd3locker.NewWithPrefix(client, "my-prefix")
+//		if err != nil {
+//				return nil, fmt.Errorf("Failed to create etcd locker: %v", err.Error())
+//		}
+//
+//
+//	For full control over all options, an etcd3.LockerOptions may be passed into
+//	etcd3.NewWithLockerOptions like the following example:
+//
+//		ttl := 15 // seconds
+//		options := etcd3locker.NewLockerOptions(ttl, "my-prefix")
+//		locker, err := etcd3locker.NewWithLockerOptions(client, options)
+//		if err != nil {
+//				return nil, fmt.Errorf("Failed to create etcd locker: %v", err.Error())
+//		}
+//
 // Tested on etcd 3.1/3.2/3.3
+//
 package etcd3locker
 
 import (
@@ -6,9 +47,9 @@ import (
 	"sync"
 	"time"
 
+	"github.com/tus/tusd"
 	etcd3 "go.etcd.io/etcd/clientv3"
 	"go.etcd.io/etcd/clientv3/concurrency"
-	"github.com/tus/tusd"
 )
 
 var (
