d32ffd79a058eecc94d46e312b8790e2d5cce33e
Manatsawin Hanmongkolchai <manatsawin@wongnai.com>
2019-01-30 10:41:23 +0700

    Add AccessDenied back

--- s3store/s3store.go
+++ s3store/s3store.go
@@ -374,7 +374,7 @@ func (store S3Store) GetInfo(id string) (info tusd.FileInfo, err error) {
 		Key:    store.keyWithPrefix(uploadId + ".part"),
 	})
 	if err != nil {
-		if !isAwsError(err, s3.ErrCodeNoSuchKey) && !isAwsError(err, "NotFound") && !isAwsError(err, "Forbidden") {
+		if !isAwsError(err, s3.ErrCodeNoSuchKey) && !isAwsError(err, "NotFound") && !isAwsError(err, "AccessDenied") && !isAwsError(err, "Forbidden") {
 			return info, err
 		}
 
