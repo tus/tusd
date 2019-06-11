b0be7f4cb3a05a263ff6f19b54859ea50bb5153b
Marius <maerious@gmail.com>
2019-06-11 16:29:31 +0200

    core: Remove fallback for subtests before Go 1.7

--- subtest_test.go
+++ subtest_test.go
@@ -1,35 +1,20 @@
-// +build !go1.7
-
 package tusd_test
 
 import (
-	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 )
 
-var subTestDepth = 0
-
 func SubTest(t *testing.T, name string, runTest func(*testing.T, *MockFullDataStore)) {
-	subTestDepth++
-	defer func() { subTestDepth-- }()
-	p := strings.Repeat("\t", subTestDepth)
+	t.Run(name, func(subT *testing.T) {
+		//subT.Parallel()
 
-	fmt.Println(p, "=== RUN SubTest:", name)
+		ctrl := gomock.NewController(subT)
+		defer ctrl.Finish()
 
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
+		store := NewMockFullDataStore(ctrl)
 
-	store := NewMockFullDataStore(ctrl)
-
-	runTest(t, store)
-
-	if t.Failed() {
-		fmt.Println(p, "--- FAIL SubTest:", name)
-		t.FailNow()
-	} else {
-		fmt.Println(p, "--- PASS SubTest:", name)
-	}
+		runTest(subT, store)
+	})
 }
