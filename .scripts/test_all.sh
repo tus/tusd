774fd3f313c693ffe48ce1aec039d030986ead58
Marius <maerious@gmail.com>
2019-06-16 17:13:16 +0200

    misc: Use Go modules on Travis CI

--- .scripts/test_all.sh
+++ .scripts/test_all.sh
@@ -4,20 +4,12 @@ set -e
 
 install_etcd_pkgs() {
   ETCD_VERSION="3.3.10"
-  go get -u go.etcd.io/etcd/clientv3
-  go get -u github.com/chen-anders/go-etcd-harness
   wget -q -O /tmp/etcd.tar.gz "https://github.com/etcd-io/etcd/releases/download/v$ETCD_VERSION/etcd-v$ETCD_VERSION-linux-amd64.tar.gz"
   tar xvzf /tmp/etcd.tar.gz -C /tmp
   export PATH="$PATH:/tmp/etcd-v$ETCD_VERSION-linux-amd64"
 }
 
-# Install the AWS SDK  which is explicitly not vendored
-go get -u github.com/aws/aws-sdk-go/service/s3
-go get -u github.com/aws/aws-sdk-go/aws/...
-
-go get -u github.com/prometheus/client_golang/prometheus
-
-# Install the etcd packages which are not vendored.
+# Install the etcd binary which is not vendored.
 install_etcd_pkgs
 
 go test ./pkg/...
