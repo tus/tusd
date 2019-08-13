5c74481d9143667284c68e78a4d1396c4b765e44
kiloreux <kiloreux@gmail.com>
2019-08-13 04:39:15 +0100

    Avoid contacting server

--- .scripts/deploy_kube.sh
+++ .scripts/deploy_kube.sh
@@ -11,7 +11,6 @@ __root="$(cd "$(dirname "${__dir}")" && pwd)"
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
-kubectl version
 
 #Store the new image in docker hub
 docker build --quiet -t tusproject/tusd:latest -t tusproject/tusd:$TRAVIS_COMMIT ${__root};
