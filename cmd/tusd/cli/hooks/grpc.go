1e8d7c91c478393500a9997a44e375a082c1b163
Iñigo Horcajo <i.horcajo@saltosystems.com>
2020-01-08 17:00:22 +0100

    remove stream grpc

--- cmd/tusd/cli/hooks/grpc.go
+++ cmd/tusd/cli/hooks/grpc.go
@@ -2,11 +2,8 @@ package hooks
 
 import (
 	"context"
-	"io"
-	"log"
 	"time"
 
-	"github.com/golang/protobuf/proto"
 	grpc_retry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
 	"github.com/tus/tusd/pkg/handler"
 	pb "github.com/tus/tusd/pkg/proto/v1"
@@ -18,7 +15,7 @@ type GrpcHook struct {
 	Endpoint   string
 	MaxRetries int
 	Backoff    int
-	GrpcOpts   []grpc.DialOption
+	Client     pb.HookServiceClient
 }
 
 func (g GrpcHook) Setup() error {
@@ -26,69 +23,32 @@ func (g GrpcHook) Setup() error {
 		grpc_retry.WithBackoff(grpc_retry.BackoffLinear(time.Duration(g.Backoff) * time.Second)),
 		grpc_retry.WithMax(uint(g.MaxRetries)),
 	}
-	g.GrpcOpts = []grpc.DialOption{
+	grpcOpts := []grpc.DialOption{
 		grpc.WithInsecure(),
-		grpc.WithStreamInterceptor(grpc_retry.StreamClientInterceptor(opts...)),
 		grpc.WithUnaryInterceptor(grpc_retry.UnaryClientInterceptor(opts...)),
 	}
+	conn, err := grpc.Dial(g.Endpoint, grpcOpts...)
+	if err != nil {
+		return err
+	}
+	g.Client = pb.NewHookServiceClient(conn)
 	return nil
 }
 
 func (g GrpcHook) InvokeHook(typ HookType, info handler.HookEvent, captureOutput bool) ([]byte, int, error) {
 	ctx := context.Background()
-	conn, err := grpc.Dial(g.Endpoint, g.GrpcOpts...)
-	if err != nil {
-		return nil, 0, err
-	}
-	defer conn.Close()
-	client := pb.NewHookServiceClient(conn)
-	stream, err := client.Send(ctx)
+	req := &pb.SendRequest{Hook: marshal(info)}
+	resp, err := g.Client.Send(ctx, req)
 	if err != nil {
 		if e, ok := status.FromError(err); ok {
 			return nil, int(e.Code()), err
 		}
-		return nil, 0, err
+		return nil, 2, err
 	}
-
-	var data []byte
-	var resp *pb.SendResponse
-	ctx = stream.Context()
-	done := make(chan bool)
-
-	req := &pb.SendRequest{Hook: marshal(info)}
-	if err := stream.Send(req); err != nil {
-		return nil, 0, err
+	if captureOutput {
+		return resp.Response.GetValue(), 0, err
 	}
-	if err := stream.CloseSend(); err != nil {
-		return nil, 0, err
-	}
-	go func() {
-		for {
-			resp, err := stream.Recv()
-			if err == io.EOF {
-				close(done)
-				return
-			}
-			if err != nil {
-				log.Fatalf("can not receive %v", err)
-			}
-			data, err := proto.Marshal(resp)
-			if err != nil {
-				log.Fatalf("can not marshal response %v", err)
-			}
-			log.Printf("new response %d received", data)
-		}
-	}()
-	go func() {
-		<-ctx.Done()
-		if err := ctx.Err(); err != nil {
-			log.Println(err)
-		}
-		close(done)
-	}()
-
-	<-done
-	return data, int(resp.StatusCode), err
+	return nil, 0, err
 }
 
 func marshal(info handler.HookEvent) *pb.Hook {
