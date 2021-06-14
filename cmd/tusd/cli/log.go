b50c2936a1f4094c74df3d8fbd85d30063d04c1c
Juanjo Rodriguez <jjrodrig@gmail.com>
2021-06-14 14:16:39 +0200

    cli: Change timestamp to microseconds precision in log (#489)

--- cmd/tusd/cli/log.go
+++ cmd/tusd/cli/log.go
@@ -7,8 +7,8 @@ import (
 	"github.com/tus/tusd/pkg/handler"
 )
 
-var stdout = log.New(os.Stdout, "[tusd] ", log.Ldate|log.Ltime)
-var stderr = log.New(os.Stderr, "[tusd] ", log.Ldate|log.Ltime)
+var stdout = log.New(os.Stdout, "[tusd] ", log.LstdFlags|log.Lmicroseconds)
+var stderr = log.New(os.Stderr, "[tusd] ", log.LstdFlags|log.Lmicroseconds)
 
 func logEv(logOutput *log.Logger, eventName string, details ...string) {
 	handler.LogEvent(logOutput, eventName, details...)
