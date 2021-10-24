8d653a2ca6770f9113cd5c4ba8082ab890df61bb
Marius <marius@transloadit.com>
2021-10-24 10:22:23 +0200

    Regenerate GCS API mock

--- pkg/gcsstore/gcsstore_test.go
+++ pkg/gcsstore/gcsstore_test.go
@@ -15,7 +15,7 @@ import (
 	"github.com/tus/tusd/pkg/handler"
 )
 
-// go:generate mockgen -destination=./gcsstore_mock_test.go -package=gcsstore_test github.com/tus/tusd/pkg/gcsstore GCSReader,GCSAPI
+//go:generate mockgen -destination=./gcsstore_mock_test.go -package=gcsstore_test github.com/tus/tusd/pkg/gcsstore GCSReader,GCSAPI
 
 const mockID = "123456789abcdefghijklmnopqrstuvwxyz"
 const mockBucket = "bucket"
