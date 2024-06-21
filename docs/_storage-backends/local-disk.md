8a8b5388819c5ecf2c98ed6a856ff1fba61409bc
Marius <marius@transloadit.com>
2024-06-21 09:51:01 +0200

    filestore: Allow customising the storage path (#1083)
    * filestore: Allow customising the destination path
    
    * Compatible with uploads from previous version
    
    * Add todo about documentation
    
    * More comments
    
    * Documentation

--- docs/_storage-backends/local-disk.md
+++ docs/_storage-backends/local-disk.md
@@ -17,6 +17,25 @@ When a new upload is created, for example with the upload ID `abcdef123`, tusd c
 - `./uploads/abcdef123` to hold the file content that is uploaded
 - `./uploads/abcdef123.info` to hold [meta information about the upload]({{ site.baseurl }}/storage-backends/overview/#storage-format)
 
+## Custom storage location
+
+The locations of the two files mentioned above can be fully customized using the [pre-create hook](({ site.baseurl }}/advanced-topics/hooks/). The location of the `.info` file is derived from the upload ID, which can be customized by the pre-create hook using the [`ChangeFileInfo.ID` setting]({ site.baseurl }}/advanced-topics/hooks/#hook-requests-and-responses). Similarly, the location where the file content is saved is by default derived from the upload ID, but can be fully customized using the [`ChangeFileInfo.Storage.Path` setting]({ site.baseurl }}/advanced-topics/hooks/#hook-requests-and-responses).
+
+For example, if the pre-create hook returns the following hook response, an upload with ID `project-123/abc` is created, the info file is saved at `./uploads/project-123/abc.info`, and the file content is saved at `./uploads/project-123/abc/presentation.pdf`:
+
+```js
+{
+    "ChangeFileInfo": {
+        "ID": "project-123/abc",
+        "Storage": {
+            "Path": "project-123/abc/presentation.pdf"
+        }
+    },
+}
+```
+
+If the defined path is relative, it will be resolved from the directory defined using `-dir`.
+
 ## Issues with NFS and shared folders
 
 Tusd maintains [upload locks]({{ site.baseurl }}/advanced-topics/locks/) on disk to ensure exclusive access to uploads and prevent data corruption. These disk-based locks utilize hard links, which might not be supported by older NFS versions or when a folder is shared in a VM using VirtualBox or Vagrant. In these cases, you might get errors like this:
--- pkg/filestore/filestore.go
+++ pkg/filestore/filestore.go
@@ -56,7 +56,17 @@ func (store FileStore) NewUpload(ctx context.Context, info handler.FileInfo) (ha
 	if info.ID == "" {
 		info.ID = uid.Uid()
 	}
-	binPath := store.binPath(info.ID)
+
+	// The .info file's location can directly be deduced from the upload ID
+	infoPath := store.infoPath(info.ID)
+	// The binary file's location might be modified by the pre-create hook.
+	var binPath string
+	if info.Storage != nil && info.Storage["Path"] != "" {
+		binPath = filepath.Join(store.Path, info.Storage["Path"])
+	} else {
+		binPath = store.defaultBinPath(info.ID)
+	}
+
 	info.Storage = map[string]string{
 		"Type": "filestore",
 		"Path": binPath,
@@ -69,7 +79,7 @@ func (store FileStore) NewUpload(ctx context.Context, info handler.FileInfo) (ha
 
 	upload := &fileUpload{
 		info:     info,
-		infoPath: store.infoPath(info.ID),
+		infoPath: infoPath,
 		binPath:  binPath,
 	}
 
@@ -82,8 +92,8 @@ func (store FileStore) NewUpload(ctx context.Context, info handler.FileInfo) (ha
 }
 
 func (store FileStore) GetUpload(ctx context.Context, id string) (handler.Upload, error) {
-	info := handler.FileInfo{}
-	data, err := os.ReadFile(store.infoPath(id))
+	infoPath := store.infoPath(id)
+	data, err := os.ReadFile(infoPath)
 	if err != nil {
 		if os.IsNotExist(err) {
 			// Interpret os.ErrNotExist as 404 Not Found
@@ -91,12 +101,23 @@ func (store FileStore) GetUpload(ctx context.Context, id string) (handler.Upload
 		}
 		return nil, err
 	}
+	var info handler.FileInfo
 	if err := json.Unmarshal(data, &info); err != nil {
 		return nil, err
 	}
 
-	binPath := store.binPath(id)
-	infoPath := store.infoPath(id)
+	// If the info file contains a custom path to the binary file, we use that. If not, we
+	// fall back to the default value (although the Path property should always be set in recent
+	// tusd versions).
+	var binPath string
+	if info.Storage != nil && info.Storage["Path"] != "" {
+		// No filepath.Join here because the joining already happened in NewUpload. Duplicate joining
+		// with relative paths lead to incorrect paths
+		binPath = info.Storage["Path"]
+	} else {
+		binPath = store.defaultBinPath(info.ID)
+	}
+
 	stat, err := os.Stat(binPath)
 	if err != nil {
 		if os.IsNotExist(err) {
@@ -127,8 +148,9 @@ func (store FileStore) AsConcatableUpload(upload handler.Upload) handler.Concata
 	return upload.(*fileUpload)
 }
 
-// binPath returns the path to the file storing the binary data.
-func (store FileStore) binPath(id string) string {
+// defaultBinPath returns the path to the file storing the binary data, if it is
+// not customized using the pre-create hook.
+func (store FileStore) defaultBinPath(id string) string {
 	return filepath.Join(store.Path, id)
 }
 
